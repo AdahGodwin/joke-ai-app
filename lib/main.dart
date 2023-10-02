da9af184a7c524c6b0642a2fbb1f62b1206f64a1
Phosah <efosa.uyi@gmail.com>
2023-10-02 18:28:45 +0100

    create scrolling cards widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Jokes AI',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
