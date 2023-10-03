9f5f570dd3ff277107943f5751e8d47b3f01a895
Phosah <efosa.uyi@gmail.com>
2023-10-03 09:29:27 +0100

    overflow fix and appropriate spacing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,58 +19,69 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Jokes AI'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('Jokes AI'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.grey[100],
+                  ),
+                  padding: const EdgeInsets.all(4.0),
+                  child: IconButton(
+                    icon: Image.asset('assets/icons/menu-icon.png'),
+                    onPressed: isDrawerOpen ? closeDrawer : openDrawer,
+                  ),
                 ),
-                padding: const EdgeInsets.all(4.0),
-                child: IconButton(
-                  icon: Image.asset('assets/icons/menu-icon.png'),
-                  onPressed: isDrawerOpen ? closeDrawer : openDrawer,
+                const Text('Good Morning Godwin!'),
+                const Text('You have several jokes, pick below to continue'),
+                const SizedBox(
+                  height: 20.0,
                 ),
-              ),
-              const Text('Good Morning Godwin!'),
-              const Text('You have several jokes, pick below to continue'),
-              const SizedBox(
-                height: 20.0,
-              ),
-              ScrollingCards(),
-              const SizedBox(
-                height: 20.0,
-              ),
-              const Text('Suggested Jokes'),
-              const Text('Ask for a joke and get a cool response'),
-              const SizedBox(
-                height: 20,
-              ),
-              Expanded(child: JokeSuggestion()),
-              Container(
+                ScrollingCards(),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const Text('Suggested Jokes'),
+                const Text('Ask for a joke and get a cool response'),
+                const SizedBox(
+                  height: 20,
+                ),
+                Expanded(child: JokeSuggestion()),
+                const SizedBox(
+                  height: 80,
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
                 margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
                 height: 60,
                 padding: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1,
-                  ),
-                ),
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                    color: Colors.white),
                 child: NewMessage(
                   chatId: selectedChatId,
                 ),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
